a63940066eaa318b9c2f88689fb92985082de9c6
Camilo666 <dcmr34012@gmail.com>
2023-06-12 14:09:50 -0500

    agregando mensaje producto creado

--- jscript.js
+++ jscript.js
@@ -1,13 +1,14 @@
-
-
-
-
-$('#crear').on("click",function() {
-    Mensaje();
-    
-});
+const nombre=document.getElementById("Name");
+const precio=document.getElementById("Price");
+const inventario=document.getElementById("Inventory");
+const form=document.getElementById("form");
 
 function Mensaje(){
-
     document.write("El producto fue creado");
+
 }
+
+form.addEventListener("submit",Mensaje ,true);
+
+
+
